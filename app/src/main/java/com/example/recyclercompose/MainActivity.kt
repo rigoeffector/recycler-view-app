52bc7e3224b7f89451bc060cd04c4b6a82142fff
rigorene48 <rigorene48@gmail.com>
2024-07-17 20:30:47 +0200

    Removed Comments

--- app/src/main/java/com/example/recyclercompose/MainActivity.kt
+++ app/src/main/java/com/example/recyclercompose/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.recyclercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.recyclercompose.navigation.NavGraph
 import com.example.recyclercompose.ui.theme.RecyclerComposeTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             RecyclerComposeTheme {
-                val navController = rememberNavController()
-                NavGraph(navController = navController)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Transparent
+                ) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
             }
         }
     }
--- app/src/main/java/com/example/recyclercompose/navigation/NavGraph.kt
+++ app/src/main/java/com/example/recyclercompose/navigation/NavGraph.kt
@@ -4,12 +4,10 @@ import ListViewScreen
 import LoadingIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.recyclercompose.screens.SplashScreen
 import com.example.recyclercompose.screens.DetailsViewScreen
 import com.example.recyclercompose.viewmodel.DataViewModel
 
@@ -17,10 +15,8 @@ import com.example.recyclercompose.viewmodel.DataViewModel
 fun NavGraph(navController: NavHostController) {
     val viewModel: DataViewModel = viewModel()
 
-    NavHost(navController = navController, startDestination = "splashScreen") {
-        composable("splashScreen") {
-            SplashScreen(navController)
-        }
+    NavHost(navController = navController, startDestination = "listViewScreen") {
+
         composable("listViewScreen") {
             val dataState = viewModel.data.collectAsState()
             val isLoading = viewModel.isLoading.collectAsState()
