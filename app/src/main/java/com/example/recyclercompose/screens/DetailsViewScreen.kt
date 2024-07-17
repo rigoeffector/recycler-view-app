5c9ed697b55b11560cbf33be9887bece90101cd8
rigorene48 <rigorene48@gmail.com>
2024-07-17 22:34:04 +0200

    Details Screen Modified Styles

--- app/src/main/java/com/example/recyclercompose/screens/DetailsViewScreen.kt
+++ app/src/main/java/com/example/recyclercompose/screens/DetailsViewScreen.kt
@@ -1,19 +1,27 @@
-
 package com.example.recyclercompose.screens
+
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.recyclercompose.models.DataModel
 import com.example.recyclercompose.ui.theme.ampersand
 
 @Composable
-fun DetailsViewScreen(item: DataModel,  navController: NavController) {
+fun DetailsViewScreen(item: DataModel, navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -21,7 +29,6 @@ fun DetailsViewScreen(item: DataModel,  navController: NavController) {
                 navigationIcon = {
                     IconButton(
                         onClick = { navController.popBackStack() },
-
                     ) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "Back", tint = ampersand)
                     }
@@ -30,20 +37,40 @@ fun DetailsViewScreen(item: DataModel,  navController: NavController) {
                 elevation = AppBarDefaults.TopAppBarElevation
             )
         }
-    ) {
-        Column(
+    ) { paddingValues ->
+        Box(
             modifier = Modifier
-                .fillMaxSize()
+                .padding(paddingValues)
                 .padding(16.dp)
         ) {
-            Text(text = item.name, style = MaterialTheme.typography.h4)
-            Spacer(modifier = Modifier.height(8.dp))
-            item.data?.forEach { (key, value) ->
-                Text(text = "$key: $value", style = MaterialTheme.typography.body1)
-                Spacer(modifier = Modifier.height(4.dp))
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Gray, RoundedCornerShape(10.dp))
+                    .padding(16.dp)
+            ) {
+                Text(text = item.name, style = MaterialTheme.typography.h4)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider(color = Color.Gray, thickness = 1.dp)
+                Spacer(modifier = Modifier.height(8.dp))
+                item.data?.forEach { (key, value) ->
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(
+                                color = ampersand,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 18.sp // Adjust this value as needed
+                            )) {
+                                append(key.uppercase())
+                            }
+                            append(" : $value")
+                        },
+                        style = MaterialTheme.typography.body1
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                }
             }
         }
     }
-
-
 }
